96fda321e287eef59633d86ba379561a2dc6a6e0
LeeKangRyong <gaiogo2@naver.com>
2026-01-10 12:41:17 +0900

    fix(App): export {} to default

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { Input, Output } from "./views/input/Input";
-import { Domain } from "../models/index.js";
-import  { DomainService, ViewService } from "./services/DomainService.js";
+// import { Input, Output } from "./views/index.js";
+// import { Domain } from "../models/index.js";
+// import  { DomainService, ViewService } from "./services/DomainService.js";
 
 class App {
   async run() {
     // 메인 컨트롤러로 작동
+    console.log(2);
   }
 }
 
-export { App };
+export default App;
